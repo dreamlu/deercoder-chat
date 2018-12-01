df6657a540c9066fc0cdeb92b8cfcc43dcc72ea4
Dreamlu <3147460271@qq.com>
2018-12-01 18:10:21 +0800

    update

---
+++ chat/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	//静态目录
+	r.Static("/static", "static")
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8007") // listen and serve on 0.0.0.0:8080
+}
